2f8db72fec3dde59429c9ccaae973db2df9b154b
epicWolfLP <128297322+epicWolfLP@users.noreply.github.com>
2025-02-08 22:33:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,60 @@ document.getElementById("inputField").addEventListener("keypress", function(even
 
         if (input === "party") {
             document.body.classList.add("party-mode");
+            removeMedia();
+        }
+
+        else if (input === "taco bell") {
+            removeMedia();
+            showVideo("videos/TACOBELL!!!.mp4");
+        }
+
+        else if (input === "backshot") {
+            removeMedia();
+            showGif("images/backshot.gif");
+        }
+        else if (input === "stop") {
+            removeMedia();
+            document.body.classList.remove("party-mode");
         }
 
         this.value = "";
     }
 });
+function removeMedia() {
+    let existingMedia = document.getElementById("backgroundMedia");
+    if (existingMedia) {
+        existingMedia.remove();
+    }
+}
+
+function showVideo(videoSrc) {
+    let video = document.createElement("video");
+    video.id = "backgroundMedia";
+    video.src = videoSrc;
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true;
+    video.style.position = "fixed";
+    video.style.top = "0";
+    video.style.left = "0";
+    video.style.width = "100vw";
+    video.style.height = "100vh";
+    video.style.objectFit = "cover";
+    video.style.zIndex = "-1";
+    document.body.appendChild(video);
+}
+
+function showGif(gifSrc) {
+    let img = document.createElement("img");
+    img.id = "backgroundMedia";
+    img.src = gifSrc;
+    img.style.position = "fixed";
+    img.style.top = "0";
+    img.style.left = "0";
+    img.style.width = "100vw";
+    img.style.height = "100vh";
+    img.style.objectFit = "cover";
+    img.style.zIndex = "-1";
+    document.body.appendChild(img);
+}
